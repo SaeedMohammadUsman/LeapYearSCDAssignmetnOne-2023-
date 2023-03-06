3280fc9657e0e7ad1836d7dc316649258dab017e
Mohammad Usman <125744060+SaeedMohammadUsman@users.noreply.github.com>
2023-03-07 00:21:58 +0500

    Assignment 1 SCD (2023)

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -9,15 +9,20 @@
 //////////////////////////////////////////////////////////////////////////
 
 import javax.swing.JOptionPane;
+
 public class LeapYear {
     public static void main(String[] args) {
-
-        for (int i = 0; i < 5; i++) {
-            //take five time an input number for checking
+        for (int i = 0; i < 5; i++) { //take five time an input number for checking
             String inputYear = JOptionPane.showInputDialog(null, "Enter a year to check if it is a leap year:");
-            int year = Integer.parseInt(inputYear);// convert the string to the int (parseInt)
 
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){  //condition
+            // Ensure the user inputs a year with 4 digits and is greater than or equal to 1900
+            while (inputYear.length() != 4 || Integer.parseInt(inputYear) < 1900) {
+                inputYear = JOptionPane.showInputDialog(null, "Invalid input. Enter a year to check if it is a leap year (4 digits, >= 1900):");
+            }
+
+            int year = Integer.parseInt(inputYear); // convert the string to the int (parseInt)
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
                 JOptionPane.showMessageDialog(null, year + " is a leap year.");
             } else {
                 JOptionPane.showMessageDialog(null, year + " is not a leap year.");
@@ -25,3 +30,5 @@ public class LeapYear {
         }
     }
 }
+
+
